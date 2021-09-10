1f6cea0ff2b12580e2695e11ebe45286e037b9d8
Akr1s <dimakr71@gmail.com>
2021-09-11 00:31:55 +0300

    refactor(dishes): replaced duplicated code with helpers

--- server/controlers/dishes.controler.js
+++ server/controlers/dishes.controler.js
@@ -1,93 +1,74 @@
-const db = require("../config/database");
+const {
+  getHelper,
+  addHelper,
+  updateHelper,
+  deleteHelper,
+} = require("../helpers/index");
 
-exports.getAll = async (req, res) => {
-  try {
-    const { rows } = await db.query("SELECT * FROM DISHES");
-    res.status(200).send(rows);
-  } catch (error) {
-    res
-      .status(500)
-      .send({ message: "Dishes getAll error", info: error.message });
-  }
+exports.getAll = (req, res) => {
+  getHelper("SELECT * FROM DISHES", "Dishes getAll error", res, false);
 };
 
-exports.add = async (req, res) => {
+exports.getSingle = (req, res) => {
+  const { id } = req.params;
+  getHelper(
+    `SELECT *  FROM DISHES WHERE ID=${id}`,
+    "Dishes delete error",
+    res,
+    true
+  );
+};
+
+exports.getAllFromCategory = (req, res) => {
+  const { id } = req.params;
+  getHelper(
+    `SELECT * FROM DISHES WHERE CATEGORY IN (SELECT ID FROM CATEGORIES WHERE PARENT=${id})`,
+    "Dishes getAllFromCategory error",
+    res,
+    false
+  );
+};
+
+exports.add = (req, res) => {
   const { name, description, img, show, category, weights } = req.body;
-  try {
-    const { rows } = await db.query(
-      "INSERT INTO DISHES(NAME,DESCRIPTION, IMG, SHOW, CATEGORY, WEIGHTS) VALUES ($1,$2,$3, $4, $5, $6)",
-      [name, description, img, show, category, weights]
-    );
-    res.status(201).send({ message: `Dish ${name} created!` });
-  } catch (error) {
-    res.status(500).send({ message: "Dishes add error", info: error.message });
-  }
+  addHelper(
+    `INSERT INTO DISHES(NAME, DESCRIPTION, IMG, SHOW, CATEGORY, WEIGHTS) VALUES ('${name}', '${description}', '${img}', ${show}, ${category}, '${JSON.stringify(
+      weights
+    )}')`,
+    `Dish ${name} created!`,
+    "Dishes add error",
+    res
+  );
 };
 
-exports.update = async (req, res) => {
+exports.update = (req, res) => {
   const { id } = req.params;
   const { name, description, img, show, category, weights } = req.body;
-  try {
-    const { rows } = await db.query(
-      `UPDATE DISHES SET NAME=$1 ,DESCRIPTION=$2, IMG=$3, SHOW=$4, CATEGORY=$5, WEIGHTS=$6 WHERE ID=${id}`,
-      [name, description, img, show, category, weights]
-    );
-    res.status(200).send({ message: `Dish ${name} updated!` });
-  } catch (error) {
+  updateHelper(
+    `UPDATE DISHES SET NAME='${name}' ,DESCRIPTION='${description}', IMG='${img}', SHOW=${show}, CATEGORY=${category}, WEIGHTS='${JSON.stringify(
+      weights
+    )}' WHERE ID=${id}`,
+    `Dish ${name} updated!`,
+    "Dishes update error",
     res
-      .status(500)
-      .send({ message: "Dishes update error", info: error.message });
-  }
+  );
 };
 
-exports.delete = async (req, res) => {
+exports.delete = (req, res) => {
   const { id } = req.params;
-  try {
-    const { rows } = await db.query(`DELETE FROM DISHES WHERE ID=${id}`);
-    res.status(200).send({ message: `Dish was deleted!` });
-  } catch (error) {
+  deleteHelper(
+    `DELETE FROM DISHES WHERE ID=${id}`,
+    `Dish was deleted!`,
+    "Dishes delete error",
     res
-      .status(500)
-      .send({ message: "Dishes delete error", info: error.message });
-  }
+  );
 };
 
-exports.getSingle = async (req, res) => {
-  const { id } = req.params;
-  try {
-    const { rows } = await db.query(`SELECT *  FROM DISHES WHERE ID=${id}`);
-    res.status(200).send(rows[0]);
-  } catch (error) {
+exports.deleteAll = (req, res) => {
+  deleteHelper(
+    `DELETE FROM DISHES`,
+    `All dishes were deleted!`,
+    "Dishes deleteAll error",
     res
-      .status(500)
-      .send({ message: "Dishes delete error", info: error.message });
-  }
-};
-
-exports.deleteAll = async (req, res) => {
-  try {
-    const { rows } = await db.query(`DELETE FROM DISHES`);
-    res.status(200).send({ message: `All dishes were deleted!` });
-  } catch (error) {
-    res
-      .status(500)
-      .send({ message: "Dishes deleteAll error", info: error.message });
-  }
-};
-
-exports.getAllFromCategory = async (req, res) => {
-  const { id } = req.params;
-  try {
-    const { rows } = await db.query(
-      `SELECT * FROM DISHES WHERE CATEGORY IN (SELECT ID FROM CATEGORIES WHERE PARENT=${id})`
-    );
-    res.status(200).send(rows);
-  } catch (error) {
-    res
-      .status(500)
-      .send({
-        message: "Dishes getAllFromCategory error",
-        info: error.message,
-      });
-  }
+  );
 };
