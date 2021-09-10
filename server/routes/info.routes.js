95316070f59e29562be353529da42765b8abd1c5
Akr1s <dimakr71@gmail.com>
2021-09-10 21:27:03 +0300

    fix: fixed order of info routes

--- server/routes/info.routes.js
+++ server/routes/info.routes.js
@@ -2,12 +2,9 @@ const router = require("express-promise-router")();
 
 const controler = require("../controlers/info.controler");
 
-router.get("/", controler.getAll);
-
 router.get("/title", controler.getTitle);
-
 router.get("/rest", controler.getRestInfo);
-
+router.get("/", controler.getAll);
 router.put("/", controler.updateInfo);
 
 module.exports = router;
