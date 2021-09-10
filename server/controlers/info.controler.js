d8dbc5767b6df59aa54a8640423c15edf6370bb5
Akr1s <dimakr71@gmail.com>
2021-09-11 00:30:25 +0300

    refactor(info): replaced duplicated code with helpers

--- server/controlers/info.controler.js
+++ server/controlers/info.controler.js
@@ -1,47 +1,28 @@
-const db = require("../config/database");
+const { getHelper, updateHelper } = require("../helpers/index");
 
-exports.getAll = async (req, res) => {
-  try {
-    const { rows } = await db.query("SELECT * FROM INFO LIMIT 1");
-    res.status(200).send(rows[0]);
-  } catch (error) {
-    res.status(500).send({ message: "Info getAll error", info: error.message });
-  }
+exports.getAll = (req, res) => {
+  getHelper("SELECT * FROM INFO LIMIT 1", "Info getAll error", res, true);
 };
 
-exports.getTitle = async (req, res) => {
-  try {
-    const { rows } = await db.query("SELECT TITLE FROM INFO");
-    res.status(200).send(rows[0]);
-  } catch (error) {
-    res
-      .status(500)
-      .send({ message: "Info getTitle error", info: error.message });
-  }
+exports.getTitle = (req, res) => {
+  getHelper("SELECT TITLE FROM INFO", "Info getTitle error", res, true);
 };
 
-exports.getRestInfo = async (req, res) => {
-  try {
-    const { rows } = await db.query("SELECT ADDRESS, TEL, WIFI FROM INFO");
-    res.status(200).send(rows[0]);
-  } catch (error) {
-    res
-      .status(500)
-      .send({ message: "Info getRestInfo error", info: error.message });
-  }
+exports.getRestInfo = (req, res) => {
+  getHelper(
+    "SELECT ADDRESS, TEL, WIFI FROM INFO",
+    "Info getRestInfo error",
+    res,
+    true
+  );
 };
 
-exports.updateInfo = async (req, res) => {
-  try {
-    const { title, address, tel, wifi } = req.body;
-    const { rows } = db.query(
-      `UPDATE INFO SET TITLE=$1, ADDRESS=$2, TEL=$3, WIFI=$4`,
-      [title, address, tel, wifi]
-    );
-    res.status(200).send({ message: "Information was succesfully updated!" });
-  } catch (error) {
+exports.updateInfo = (req, res) => {
+  const { title, address, tel, wifi } = req.body;
+  updateHelper(
+    `UPDATE INFO SET TITLE='${title}', ADDRESS='${address}', TEL='${tel}', WIFI='${wifi}'`,
+    "Information was succesfully updated!",
+    "Info updateInfo error",
     res
-      .status(500)
-      .send({ message: "Info updateInfo error", info: error.message });
-  }
+  );
 };
