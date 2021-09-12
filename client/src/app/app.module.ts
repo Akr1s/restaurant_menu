d9099d44f6fc075c9568dac2ed2e13854b91bce0
Akr1s <dimakr71@gmail.com>
2021-09-12 19:24:50 +0300

    feat: created footer

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -7,9 +7,10 @@ import { Menu } from './pages/menu/menu.component';
 import { Admin } from './pages/admin/admin.component';
 import { Header } from './components/shared/header/header.component';
 import { Error404 } from './components/shared/error404/error404.component';
+import { FooterComponent } from './components/shared/footer/footer.component';
 
 @NgModule({
-  declarations: [AppComponent, Admin, Menu, Header, Error404],
+  declarations: [AppComponent, Admin, Menu, Header, Error404, FooterComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
