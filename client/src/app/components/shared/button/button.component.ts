9287112dcdd7098baff5bf5ec8a8cee5033e3146
Akr1s <dimakr71@gmail.com>
2021-09-27 11:19:46 +0300

    feat: added shared button component

--- client/src/app/components/shared/button/button.component.ts
+++ client/src/app/components/shared/button/button.component.ts
@@ -7,6 +7,15 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class ButtonComponent implements OnInit {
   @Input() btnText: string;
+  @Input() ok: boolean = false;
+  @Input() submit: boolean = false;
+  @Input() cancel: boolean = false;
+  @Input() small: boolean = false;
+  @Input() add: boolean = false;
+  @Input() close: boolean = false;
+  @Input() largeFont: boolean = false;
+  @Input() isDisabled: boolean = false;
+
   constructor() {}
 
   ngOnInit(): void {}
